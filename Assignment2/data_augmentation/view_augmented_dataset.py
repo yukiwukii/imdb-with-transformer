551fd35310261afa363394988e3af7e87e7a6c4e
yukiwuki <131858939+yukiwukii@users.noreply.github.com>
2025-04-12 00:23:31 +0800

    removed unecessary folders

--- Assignment2/data_augmentation/view_augmented_dataset.py
+++
@@ -1,8 +0,0 @@
-from datasets import load_dataset
-
-# Load the IMDB dataset
-dataset = load_dataset("HamsterShiu/IMDB_Combined")
-
-offset = 1000
-print(dataset["train"][0 + offset])
-print(dataset["train"][25000 + offset])
